69ebef77b7f4622c10d3139b36cd65af82e969ed
sarajalal2004 <sarauobeducation@gmail.com>
2026-03-30 19:54:30 +0300

    make folder

---
+++ src/main/java/ga/jdb/FirefighterSorter/FirefighterSorter/FirefighterSorterApplication.java
@@ -0,0 +1,13 @@
+package ga.jdb.FirefighterSorter.FirefighterSorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirefighterSorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirefighterSorterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ga/jdb/FirefighterSorter/FirefighterSorter/FirefighterSorterApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.jdb.FirefighterSorter.FirefighterSorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirefighterSorterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
