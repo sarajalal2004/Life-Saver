22235aa455e24c03fbe9b4663092fe5be8e09881
sarajalal2004 <sarauobeducation@gmail.com>
2026-04-06 19:52:41 +0300

    Create MyUserDetails

---
+++ src/main/java/ga/jdb/FirefighterSorter/FirefighterSorter/security/MyUserDetails.java
@@ -0,0 +1,60 @@
+package ga.jdb.FirefighterSorter.FirefighterSorter.security;
+
+import ga.jdb.FirefighterSorter.FirefighterSorter.model.User;
+import lombok.Getter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+@Getter
+public class MyUserDetails implements UserDetails {
+    private User user;
+
+    @Autowired
+    public void setUser(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(
+                new SimpleGrantedAuthority("ROLE_" + user.getRole().name())
+        );
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.getStatus().equals(User.Status.Active);
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
