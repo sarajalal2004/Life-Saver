7a80bcd4c01f50b9e3fca06ed4567d1262208505
sarajalal2004 <sarauobeducation@gmail.com>
2026-04-14 19:54:12 +0300

    create transport service and controllers

---
+++ src/main/java/ga/jdb/FirefighterSorter/FirefighterSorter/controller/TransportController.java
@@ -0,0 +1,57 @@
+package ga.jdb.FirefighterSorter.FirefighterSorter.controller;
+
+import ga.jdb.FirefighterSorter.FirefighterSorter.model.Transport;
+import ga.jdb.FirefighterSorter.FirefighterSorter.repository.TransportRepository;
+import ga.jdb.FirefighterSorter.FirefighterSorter.service.TransportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api")
+public class TransportController {
+    private TransportService transportService;
+
+    @Autowired
+    public void setTransportService(TransportService transportService){
+        this.transportService = transportService;
+    }
+
+    @GetMapping(path = "/transports")
+    public List<Transport> getAllTransports(){
+        return transportService.getAllTransports();
+    }
+
+    @GetMapping(path = "/branch/{branchId}/transports")
+    public List<Transport> getTransports(@PathVariable("branchId") Long branchId){
+        return transportService.getTransports(branchId);
+    }
+
+    @GetMapping(path = "/branch/{branchId}/transports/{transportId}")
+    public Transport getTransport(@PathVariable("branchId") Long branchId,
+                                  @PathVariable("transportId") Long transportId){
+        return transportService.getTransport(branchId, transportId);
+    }
+
+    @PostMapping(path = "/branch/{branchId}/transports")
+    public Transport createTransport(@PathVariable("branchId") Long branchId, @RequestBody Transport transport){
+        return transportService.createTransport(branchId, transport);
+    }
+
+    @PutMapping(path = "/branch/{branchId}/transports/{transportId}")
+    public Transport updateTransport(@PathVariable("branchId") Long branchId,
+                                     @PathVariable("transportId") Long transportId,
+                                     @RequestBody Transport transport){
+        return transportService.updateTransport(branchId, transportId, transport);
+    }
+
+    @DeleteMapping(path = "/branch/{branchId}/transports/{transportId}")
+    public Transport deleteTransport(@PathVariable("branchId") Long branchId,
+                                     @PathVariable("transportId") Long transportId){
+        return transportService.deleteTransport(branchId, transportId);
+    }
+
+    //TODO: transfer transport functionality
+
+}
