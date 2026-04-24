c70f76a372dbc58b6a014df1aa17b3947961063e
sarajalal2004 <sarauobeducation@gmail.com>
2026-04-24 20:39:09 +0300

    final clone

--- src/main/java/ga/jdb/FirefighterSorter/FirefighterSorter/model/Case.java
+++ src/main/java/ga/jdb/FirefighterSorter/FirefighterSorter/model/Case.java
@@ -66,7 +66,7 @@ public class Case {
     private Boolean peopleInside;
 
     @JoinColumn(name = "suggested_branch_id")
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @JsonIgnore
     @ManyToOne
     private Branch suggestedBranch;
 
